31d9b212cb1366f01def215cbcb67c1c2e376712
prasannashree0727-ui <prasannashree0727@gmail.com>
2026-06-27 21:26:20 +0530

    Add files via upload

---
+++ Java-FSE-HandsOn/Week1/Exercise11_Logging_SLF4J/Exercise11_Logging_SLF4J/src/main/java/com/logging/Exercise11_Logging_SLF4J/App.java
@@ -0,0 +1,13 @@
+package com.logging.Exercise11_Logging_SLF4J;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
