69eb8239bd91c7603dc1b2b251cd53e4f4c1208e
prasannashree0727-ui <prasannashree0727@gmail.com>
2026-07-04 20:13:05 +0530

    Add files via upload

---
+++ Java-FSE-HandsOn/week3/Ex5_CountryBasedOnCountryCode/CountryService.java
@@ -0,0 +1,30 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                (List<Country>) context.getBean("countryList");
+
+        for (Country country : countryList) {
+            if (country.getCode().equalsIgnoreCase(code)) {
+                return country;
+            }
+        }
+
+        return null;
+    }
+}
